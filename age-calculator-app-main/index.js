2d06d9c7148edd55b7c1eebad85e50fe956de932
hexasix <qsf44@outlook.com>
2023-04-18 09:58:41 +0800

    位于分支 main 您的分支与上游分支 'origin/main' 一致。
    要提交的变更：
    	修改：     age-calculator-app-main/index.js

--- age-calculator-app-main/index.js
+++ age-calculator-app-main/index.js
@@ -5,7 +5,57 @@ const roundButton = document.querySelector(".button");
 const emptyWarningTexts = document.querySelectorAll(".empty-warning-text");
 const timestampNow = new Date().getTime();
 console.log(emptyWarningTexts);
+const validation = (day, month, year) => {
+  const currentYear = new Date().getFullYear();
+  const userInputYear = userInputDate.getFullYear();
+  //If the inputs are empty
+  let flag = true;
+  if (!day) {
+    emptyWarningTexts[0].style.display = "inline";
+    flag = false;
+  }
+  if (!month) {
+    emptyWarningTexts[1].style.display = "inline";
+    flag = false;
+  }
+  if (!year) {
+    emptyWarningTexts[2].style.display = "inline";
+    flag = false;
+  }
+  if (!day || !month || !day) {
+    roundButton.style.top = "7.5rem";
+    flag = false;
+  }
+  if (month < 1 || month > 12) {
+    const invalidWarningParagraph = document.createElement("p");
+    const monthInputFlexbox = document.querySelector("div:has(>#month)");
+    invalidWarningParagraph.textContent = "Invalid Month";
+    // invalidWarningParagraph.style.color = "hsl(var(--primary-color-light-red))"
+    invalidWarningParagraph.classList.add("empty-warning-text");
+    monthInputFlexbox.appendChild(invalidWarningParagraph);
+    flag = false;
+  }
+  if (day < 1 || day > 31) {
+    const invalidWarningParagraph = document.createElement("p");
+    const dayInputFlexbox = document.querySelector("div:has(>#day)");
+    invalidWarningParagraph.textContent = "Invalid Day";
+    invalidWarningParagraph.classList.add("empty-warning-text");
+    dayInputFlexbox.appendChild(invalidWarningParagraph);
+    flag = false;
+  }
+  if (userInputYear > currentYear) {
+    const invalidWarningParagraph = document.createElement("p");
+    const yearInputFlexbox = document.querySelector("div:has(>#year)");
+    invalidWarningParagraph.textContent = "Invalid Year";
+    invalidWarningParagraph.classList.add("empty-warning-text");
+    yearInputFlexbox.appendChild(invalidWarningParagraph);
+    flag = false;
+  }
+  return flag;
+};
+const calculateDateDiff = (userInputDate) =>{
 
+}
 /**
  * These are the value that users put
  */
@@ -40,41 +90,7 @@ yearInput.oninput = (event) => {
 
 roundButton.onclick = (event) => {
   event.stopPropagation();
-  const currentYear = new Date().getFullYear();
-  const userInputYear = userInputDate.getFullYear();
-  //If the inputs are empty
-  if (!day) {
-    emptyWarningTexts[0].style.display = "inline";
-  }
-  if (!month) {
-    emptyWarningTexts[1].style.display = "inline";
-  }
-  if (!year) {
-    emptyWarningTexts[2].style.display = "inline";
-  }
-  if (!day || !month || !day) {
-    roundButton.style.top = "7.5rem";
-  }
-  if (month < 1 || month > 12) {
-    const invalidWarningParagraph = document.createElement("p");
-    const monthInputFlexbox = document.querySelector("div:has(>#month)");
-    invalidWarningParagraph.textContent = "Invalid Month";
-    // invalidWarningParagraph.style.color = "hsl(var(--primary-color-light-red))"
-    invalidWarningParagraph.classList.add("empty-warning-text");
-    monthInputFlexbox.appendChild(invalidWarningParagraph);
-  }
-  if (day < 1 || day > 31) {
-    const invalidWarningParagraph = document.createElement("p");
-    const dayInputFlexbox = document.querySelector("div:has(>#day)");
-    invalidWarningParagraph.textContent = "Invalid Day";
-    invalidWarningParagraph.classList.add("empty-warning-text");
-    dayInputFlexbox.appendChild(invalidWarningParagraph);
-  }
-  if (userInputYear>currentYear) {
-    const invalidWarningParagraph = document.createElement("p");
-    const yearInputFlexbox = document.querySelector("div:has(>#year)");
-    invalidWarningParagraph.textContent = "Invalid Year";
-    invalidWarningParagraph.classList.add("empty-warning-text");
-    yearInputFlexbox.appendChild(invalidWarningParagraph);
+  if(validation(day,month,year)){
+    calculateDateDiff(userInputDate);
   }
 };
