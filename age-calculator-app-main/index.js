3c94e0cf4079fd695c43e5733348dd7cf632faab
hexasix <qsf44@outlook.com>
2023-04-18 09:41:58 +0800

    位于分支 main 您的分支与上游分支 'origin/main' 一致。
    要提交的变更：
    	修改：     age-calculator-app-main/index.js

--- age-calculator-app-main/index.js
+++ age-calculator-app-main/index.js
@@ -5,6 +5,7 @@ const roundButton = document.querySelector(".button");
 const emptyWarningTexts = document.querySelectorAll(".empty-warning-text");
 const timestampNow = new Date().getTime();
 console.log(emptyWarningTexts);
+
 /**
  * These are the value that users put
  */
@@ -18,27 +19,29 @@ let userInputDate;
 dayInput.oninput = (event) => {
   day = parseInt(event.target.value);
   if (day && month && year) {
-    userInputDate = new Date(year, month, day).getTime();
+    userInputDate = new Date(year, month, day);
     console.log("userInputDate", userInputDate);
   }
 };
 monthInput.oninput = (event) => {
   month = parseInt(event.target.value);
   if (day && month && year) {
-    userInputDate = new Date(year, month, day).getTime();
+    userInputDate = new Date(year, month, day);
     console.log("userInputDate", userInputDate);
   }
 };
 yearInput.oninput = (event) => {
   year = parseInt(event.target.value);
   if (day && month && year) {
-    userInputDate = new Date(year, month, day).getTime();
+    userInputDate = new Date(year, month, day);
     console.log("userInputDate", userInputDate);
   }
 };
 
 roundButton.onclick = (event) => {
   event.stopPropagation();
+  const currentYear = new Date().getFullYear();
+  const userInputYear = userInputDate.getFullYear();
   //If the inputs are empty
   if (!day) {
     emptyWarningTexts[0].style.display = "inline";
@@ -51,5 +54,27 @@ roundButton.onclick = (event) => {
   }
   if (!day || !month || !day) {
     roundButton.style.top = "7.5rem";
+  }
+  if (month < 1 || month > 12) {
+    const invalidWarningParagraph = document.createElement("p");
+    const monthInputFlexbox = document.querySelector("div:has(>#month)");
+    invalidWarningParagraph.textContent = "Invalid Month";
+    // invalidWarningParagraph.style.color = "hsl(var(--primary-color-light-red))"
+    invalidWarningParagraph.classList.add("empty-warning-text");
+    monthInputFlexbox.appendChild(invalidWarningParagraph);
+  }
+  if (day < 1 || day > 31) {
+    const invalidWarningParagraph = document.createElement("p");
+    const dayInputFlexbox = document.querySelector("div:has(>#day)");
+    invalidWarningParagraph.textContent = "Invalid Day";
+    invalidWarningParagraph.classList.add("empty-warning-text");
+    dayInputFlexbox.appendChild(invalidWarningParagraph);
+  }
+  if (userInputYear>currentYear) {
+    const invalidWarningParagraph = document.createElement("p");
+    const yearInputFlexbox = document.querySelector("div:has(>#year)");
+    invalidWarningParagraph.textContent = "Invalid Year";
+    invalidWarningParagraph.classList.add("empty-warning-text");
+    yearInputFlexbox.appendChild(invalidWarningParagraph);
   }
 };
